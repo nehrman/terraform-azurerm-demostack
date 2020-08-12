1886ed219ffeef3c8199e0f022cf7f5317279270
nehrman <nicolas.ehrman@gmail.com>
2020-08-12 16:05:03 +0200

    Uncommenting remote backend

--- main.tf
+++ main.tf
@@ -1,16 +1,14 @@
 //--------------------------EMEA-SE_PLAYGROUND-2019-----------------------------------------
 # Using a single workspace:
-/**
 terraform {
   backend "remote" {
     hostname     = "app.terraform.io"
     organization = "emea-se-playground-2019"
     workspaces  {
-      name = "Guy-Azure-Demostack"
+      name = "NEH-Azure-Demostack"
   }
   }
 }
-**/
 
 
 // Workspace Data
